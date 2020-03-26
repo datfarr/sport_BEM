ec0226febfa964624a93ac98ad6497430f2dc532
Dinara G <datfarr@gmail.com>
2020-03-26 18:12:18 +0500

    catalog's sliders with scripts

--- js/script.js
+++ js/script.js
@@ -8,4 +8,25 @@ $(document).ready(function(){
         prevArrow: '<button style="background-color:#c9fde7; border:none;" type="button" class="slick-prev"><i style="font-size:50px; color: #31054e; " class="fas fa-arrow-circle-left"></i></button>',
         nextArrow: '<button style="background-color:#c9fde7; border:none;" type="button" class="slick-next"><i style="font-size:50px; color: #31054e;" class="fas fa-arrow-circle-right"></i></button>'
       });
+ 
+
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
+
+      function toggleSlide(item) {
+        $(item).each(function(i){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
